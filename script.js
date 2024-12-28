d2d818f9498a821f7c52e2b0805984313cfb4f7c
Tuhin Ghosh <66065760+Tuhin1904@users.noreply.github.com>
2024-12-28 14:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 function convertToRoman(num) {
   	const obj = {
-       0: ['M', 1000], 
-       1: ['CM', 900], 
-       2: ['D', 500], 
-       3: ['CD', 400], 
-        4: ['C', 100], 
-        5: ['XC', 90], 
-        6: ['L', 50], 
-        7 :['XL', 40], 
-        8 :['X', 10], 
-        9 :['IX', 9], 
-        10 :['V', 5], 
-        11 :['IV', 4], 
-        12 :['I', 1],
-    };
+    0: ['M', 1000],
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
+  };
 
 	let roman = '';
 
@@ -25,6 +19,16 @@ function convertToRoman(num) {
 			
 		}
 	}
+
+  //your code here
+	roman = roman
+    .replace('DCCCC', 'CM') // 900
+    .replace('CCCC', 'CD')  // 400
+    .replace('LXXXX', 'XC') // 90
+    .replace('XXXX', 'XL')  // 40
+    .replace('VIIII', 'IX') // 9
+    .replace('IIII', 'IV'); // 4
+  return roman;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
